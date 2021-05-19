c47c2c80bf0f746347f08795ab27acb3008b76f6
PadamSharma <56642812+PadamSharma@users.noreply.github.com>
2021-05-19 16:00:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+$(document).on("mousemove", function (e) {
+    $(".cursor").css({ left:e.clientX - 25 + "px", top:e.clientY - 25 + "px" });
+});
+
+
 $(document).ready(function () {
     $(".menu-toggle").click(function () {
         $("nav").toggleClass("active");
     });
-});
-// --- CURSOR
-$(document).on("mousemove", function (e) {
-    $(".cursor").css({ left:e.clientX - 25 + "px", top:e.clientY - 25 + "px" });
 });
